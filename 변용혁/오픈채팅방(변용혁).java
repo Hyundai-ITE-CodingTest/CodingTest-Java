e70b06825fd8cb9a753c4005d513b5be7dc5f685
byeonyh95 <42959237+byeonyh95@users.noreply.github.com>
2021-08-14 17:28:18 +0900

    20210814

--- 변용혁/오픈채팅방(변용혁).java
+++ 변용혁/오픈채팅방(변용혁).java
@@ -1,6 +1,41 @@
-class Solution {
-    public String[] solution(String[] record) {
-        String[] answer = {};
-        return answer;
+import java.util.*;
+
+public class Solution {
+	public static String[] solution(String[] record) {
+		
+		int len = record.length;
+        String[] answer = new String[len];
+        Map<String, String> map = new HashMap<String, String>();
+        int cnt = 0; //결과 갯수 카운트
+        String[][] temp = new String[len][2]; // id + 메세지 저장
+        
+        for(int i=0; i<len; i++) {
+        	String[] msg = record[i].split(" ");
+        	if (msg[0].equals("Enter")) {
+        		
+        		map.put(msg[1], msg[2]);
+        		temp[cnt][0] = msg[1];
+        		temp[cnt][1] = "님이 들어왔습니다.";
+        		cnt += 1;
+        		
+        	}else if(msg[0].equals("Change")) {     
+        		
+        		map.put(msg[1], msg[2]);
+        		
+        	}else if(msg[0].equals("Leave")) {
+        		
+        		temp[cnt][0] = msg[1];
+        		temp[cnt][1] = "님이 나갔습니다.";
+        		cnt += 1;
+        		
+        	}
+        }
+        
+        for(int i=0; i<cnt; i++) {
+        	answer[i] = map.get(temp[i][0]) + temp[i][1];
+        }
+        
+        return Arrays.copyOf(answer, cnt);
     }
+	
 }
