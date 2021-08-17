8ecefeb7fa3535c68f0b9892135e78bb545e0bcc
Seung1110 <iiwvii@naver.com>
2021-08-17 18:16:11 +0900

    #13

---
+++ 이승엽/과제1(이승엽).java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class no01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			System.out.println("Hello MyCompany!");
+		}
+
+	}
+}
