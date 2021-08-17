8412ff6e152640d60711095b7a3947f768d22431
EasyBAMM <huhjb1020@naver.com>
2021-08-17 19:49:04 +0900

    #13

---
+++ 허준범/과제1(허준범).java
@@ -0,0 +1,14 @@
+package homework1;
+
+import java.util.Scanner;
+
+public class P1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			System.out.println("Hello MyCompany!");
+		}
+	}
+}
