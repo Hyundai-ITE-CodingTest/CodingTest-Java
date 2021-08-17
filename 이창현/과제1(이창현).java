995cedd2ab09008ea1998b36967d0fdbfbe90d6e
leechang4 <79917450+leechang4@users.noreply.github.com>
2021-08-18 02:56:06 +0900

    #13

---
+++ 이창현/과제1(이창현).java
@@ -0,0 +1,16 @@
+package ars;
+
+import java.util.Scanner;
+
+public class Homework {
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int N = sc.nextInt();
+	
+	for(int i = 0; i<3; i++ ) {
+		System.out.println("Hello MyCompany!");
+		}
+		
+	}
+
+}
