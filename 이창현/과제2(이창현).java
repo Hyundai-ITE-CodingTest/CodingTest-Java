2ba967ca7c1c76d093b8cdc781936e0802e0ef03
leechang4 <79917450+leechang4@users.noreply.github.com>
2021-08-18 02:57:38 +0900

    #14

---
+++ 이창현/과제2(이창현).java
@@ -0,0 +1,30 @@
+package ars;
+
+import java.util.*;
+
+public class Homework2 {
+	public static void main(String[] args) {
+		System.out.print("입력.>> ");
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		String[] arr = s.split(","); // ,
+		int min = Integer.MAX_VALUE; // 최솟값 구하기
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			int re = Integer.parseInt(arr[i]); 
+
+			if (re % 2 == 1) {
+				sum += re;
+				min = Math.min(re, min); // 두 인자 값 중 작은 값을 리턴
+			}
+		}
+
+		if (sum == 0) {
+			System.out.println(-1);
+		} else {
+			System.out.println(sum);
+			System.out.println(min);
+		}
+
+	}
+}
