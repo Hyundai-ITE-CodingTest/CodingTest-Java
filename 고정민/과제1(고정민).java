e7a66348490a6aed137f6f640adcba101dbf46d7
Jeongmin-GO <59509452+Jeongmin-GO@users.noreply.github.com>
2021-08-17 23:46:51 +0900

    #13 20210817

---
+++ 고정민/과제1(고정민).java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class Main {
+ public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
+   int n = sc.nextInt();
+   for(int i = 0; i<n;i++){
+     System.out.println("Hello MyCompany!");
+   }
+} }
