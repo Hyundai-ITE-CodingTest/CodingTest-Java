ddb34838f6123873dd27aa5893f50363c530d85a
seung1110 <54969299+seung1110@users.noreply.github.com>
2021-08-18 07:56:27 +0900

    #16

--- 이승엽/과제4(이승엽).java
+++ 이승엽/과제4(이승엽).java
@@ -7,7 +7,7 @@ public class no04 {
 		String str = sc.nextLine();
 		String answer = "";
 		for(int i = 0; i < str.length();i++) {
-			int num = (str.charAt(i)+4)%'a';
+			int num = (str.charAt(i)-'a'+4)%26;
 			answer += (char)(num+'a');
 		}
 		System.out.println(answer);
