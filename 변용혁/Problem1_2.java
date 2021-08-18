f55b0fc02be5ef7ab4da53b53abe0eaf788004a8
byeonyh95 <42959237+byeonyh95@users.noreply.github.com>
2021-08-18 21:06:10 +0900

    20210817

---
+++ 변용혁/Problem1_2.java
@@ -0,0 +1,24 @@
+package ex01;
+
+import java.util.*;
+
+public class Problem2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		List<Integer> list = new ArrayList<Integer>();
+		int sum = 0;
+		
+		for(int i=0; i<7; i++) {
+			int temp = sc.nextInt();
+			if(temp % 2 == 1) {
+				sum += temp;
+				list.add(temp);
+			}
+		}
+		
+		System.out.println(sum);
+		System.out.println(Collections.min(list));
+		
+		
+	}
+}
