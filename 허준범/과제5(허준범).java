a3215d717e4d944681a745877b9d4a6dd92b4837
EasyBAMM <huhjb1020@naver.com>
2021-08-18 21:02:44 +0900

    #20

---
+++ 허준범/과제5(허준범).java
@@ -0,0 +1,19 @@
+package homework2;
+
+public class P1 {
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < i + 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+		for (int i = 0; i < 5; i++) {
+			for (int j = 5; j > i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
