a0c7eb97b43e7fcfe96d302ecbce6685ba8c34e6
byeonyh95 <42959237+byeonyh95@users.noreply.github.com>
2021-08-18 20:59:55 +0900

    #20

--- 변용혁/Problem1.java
+++ 변용혁/Problem1.java
@@ -1,14 +1,16 @@
-package ex01;
-
-import java.util.*;
+package ex02;
 
 public class Problem1 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		
-		for(int i=0; i<N; i++) {
-			System.out.println("Hello MyCompany!");
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=i; j++)
+				System.out.print('*');
+			System.out.println();
+		}
+		for(int i=5; i>=1; i--) {
+			for(int j=1; j<=i; j++)
+				System.out.print('*');
+			System.out.println();
 		}
 	}
 }
