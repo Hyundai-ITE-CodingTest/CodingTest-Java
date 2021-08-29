6a8816875f234c9f1ca3551bab57bd540fd4ced0
JJinTae <46085058+JJinTae@users.noreply.github.com>
2021-08-29 19:16:38 +0900

    #32

--- 김준태/균형잡힌 세상(김준태).java
+++ 김준태/균형잡힌 세상(김준태).java
@@ -23,26 +23,18 @@ public class Boj_4949 {
 
                     stack.add(temp);
                     top++;
+                    
                     if(top > 0){
-                        while(stack.get(top) - stack.get(top - 1) == 1 || stack.get(top) - stack.get(top - 1) == 2){
-
+                        if(stack.get(top) - stack.get(top - 1) == 1 || stack.get(top) - stack.get(top - 1) == 2){
                             stack.remove(top);
                             top--;
                             stack.remove(top);
                             top--;
-                            
-                            if(top < 1){
-                                break;
-                            }
                         }
                     }
                 }
             }
-            if (stack.size() == 0){
-                System.out.println("true");
-            } else {
-                System.out.println("false");
-            }
+            System.out.println(top==-1?"yes":"no");
         }
     }   
 }
