17f2e00653791507c51789c9b3520f064582f5e7
byeonyh95 <42959237+byeonyh95@users.noreply.github.com>
2021-08-30 21:38:49 +0900

    Update 오랜 기간 보호한 동물(1).sql

--- 변용혁/sql/210830/오랜 기간 보호한 동물(1).sql
+++ 변용혁/sql/210830/오랜 기간 보호한 동물(1).sql
@@ -1,6 +1,5 @@
 -- 코드를 입력하세요
 SELECT name, datetime
-    from ANIMAL_INS 
+    from (select * from ANIMAL_INS order by datetime)
     where ANIMAL_ID NOT IN (select ANIMAL_ID FROM ANIMAL_OUTS )
-    and rownum <=3
-    order by datetime;
+    and rownum <=3;
