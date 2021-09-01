4ec02bc4885b02074b0eae97fc8b62c31c03aeca
byeonyh95 <42959237+byeonyh95@users.noreply.github.com>
2021-09-01 18:46:52 +0900

    210901

---
+++ 변용혁/sql/210901/보호소에서 중성화한 동물.sql
@@ -0,0 +1,6 @@
+-- 코드를 입력하세요
+SELECT i.ANIMAL_ID, i.ANIMAL_TYPE, i.NAME
+    from ANIMAL_INS i, ANIMAL_OUTS o
+    where i.ANIMAL_ID = o.ANIMAL_ID
+    and i.SEX_UPON_INTAKE <> o.SEX_UPON_OUTCOME
+    ORDER BY i.ANIMAL_ID;
