bfe0a63b92ec41c98b653a0fb9edd6e959dadcc6
smc5720 <smc5720@gmail.com>
2021-09-04 23:32:13 +0900

    20210904

--- 서민철/백준/랜선 자르기(서민철).java
+++ 서민철/백준/랜선 자르기(서민철).java
@@ -13,7 +13,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
 
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
