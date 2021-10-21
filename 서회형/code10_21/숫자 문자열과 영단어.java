b3c6533a43efb1c8e1bccc5449b06e7f46090308
SeoHoeHyeong <shh4190@naver.com>
2021-10-21 17:47:09 +0900

    #118

---
+++ 서회형/code10_21/숫자 문자열과 영단어.java
@@ -0,0 +1,16 @@
+﻿class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        int j = 0;
+        String [] num = {"zero", "one", "two", "three", "four", "five", "six","seven","eight","nine"};
+        
+        for(String i : num){
+            s = s.replace(i,  Integer.toString(j));
+                               j++;
+        }
+        
+        answer = Integer.parseInt(s);
+        
+        return answer;
+    }
+}
