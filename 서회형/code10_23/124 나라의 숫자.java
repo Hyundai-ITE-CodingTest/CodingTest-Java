b6e4517dc90f734197ce65618a1a80e1a88731c2
SeoHoeHyeong <shh4190@naver.com>
2021-10-23 23:52:12 +0900

    #120

---
+++ 서회형/code10_23/124 나라의 숫자.java
@@ -0,0 +1,27 @@
+﻿class Solution {
+    public String solution(int n) {
+        		String answer = "";
+        int i = 9;
+        int j = 0;
+        
+        while(n != 0){
+            if (n % 3 == 0 ){
+               answer = "4" + answer; 
+                n = n/3 -1;  
+            }
+            else if (n % 3 == 1  ){
+               answer = "1" +  answer ;
+                n = n/3;
+            }
+            else if (n % 3 == 2  ){
+               answer = "2" + answer;
+                n = n/3;
+            }
+      
+            i--;
+        }
+      
+        
+        return answer;
+    }
+}
