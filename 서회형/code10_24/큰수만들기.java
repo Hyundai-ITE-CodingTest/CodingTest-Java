e1c9258711292ec3102aca03f0c9434a296ceab5
SeoHoeHyeong <shh4190@naver.com>
2021-10-24 23:49:39 +0900

    #106

---
+++ 서회형/code10_24/큰수만들기.java
@@ -0,0 +1,27 @@
+﻿import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.*;
+class Solution {
+    public String solution(String number, int k) {
+    int pos = 0;
+    char max;
+    StringBuilder answer = new StringBuilder();  
+    
+        if(number.charAt(0) =='0'){
+            return "0";
+        }
+        for(int i = 0; i < number.length() - k; i++){
+            max = '0';
+            
+            for(int j = pos; j <= k + i;j++ ){
+                if(max < number.charAt(j)){
+                    max = number.charAt(j);
+                    pos = j + 1;
+                }
+            }
+            answer.append(max);
+        }
+        return answer.toString();
+    }
+}
