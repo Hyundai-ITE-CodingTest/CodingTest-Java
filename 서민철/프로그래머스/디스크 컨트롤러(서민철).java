929196d91c3ed3821896ab853680108f3ff93a05
smc5720 <smc5720@gmail.com>
2021-11-12 11:26:50 +0900

    #103

--- 서민철/프로그래머스/디스크 컨트롤러(서민철).java
+++ 서민철/프로그래머스/디스크 컨트롤러(서민철).java
@@ -1,81 +1,63 @@
-import java.util.*;
-import java.io.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 class Solution {
-	public int solution(int[][] jobs) {
-		int jobSize = jobs.length;
+	public class Job {
+		public int start, duration;
 
-		// 시작 시간이 덜 되서 대기 중인 작업들을 저장한다.
-		PriorityQueue<Job> pendingQueue = new PriorityQueue<Job>(new Comparator<Job>() {
-			@Override
-			public int compare(Job o1, Job o2) {
-				return Integer.compare(o1.start, o2.start);
-			}
-		});
-
-		// 시작 시간은 됐으나, 이전 작업이 처리될 때까지 기다리는 작업들을 저장한다.
-		PriorityQueue<Job> processQueue = new PriorityQueue<Job>(new Comparator<Job>() {
-			@Override
-			public int compare(Job o1, Job o2) {
-				if (o1.required == o2.required) {
-					return Integer.compare(o1.start, o2.start);
-				}
-				return Integer.compare(o1.required, o2.required);
-			}
-		});
-
-		for (int[] time : jobs) {
-			Job job = new Job(time[0], time[1]);
-			pendingQueue.add(job);
+		public Job(int start, int duration) {
+			this.start = start;
+			this.duration = duration;
 		}
-
-		// 모든 작업들의 대기 시간 총합을 저장한다.
-		int sum = 0;
-
-		// 전 작업이 종료된 시간을 저장한다.
-		int prev = 0;
-
-		// 두 큐가 모두 비어야 종료된다.
-		while (!pendingQueue.isEmpty() || !processQueue.isEmpty()) {
-			// processQueue는 비었는데 prev가 작아서 pendingQueue에서 값이 넘어오지 못하는 경우
-			if (processQueue.isEmpty() && pendingQueue.peek().start > prev) {
-				prev = pendingQueue.peek().start;
-				// System.out.printf("pendingQueue에는 %d개 작업이 남아있으나 현재 시각(prev)이 %d이므로 작업을 시작할 수 없음\n", pendingQueue.size(), prev);
-			}
-
-			while (!pendingQueue.isEmpty() && pendingQueue.peek().start <= prev) {
-				processQueue.add(pendingQueue.poll());
-			}
-
-			// System.out.printf("%d에 시작 => ", prev);
-
-			Job job = processQueue.poll();
-			// System.out.printf("[%d, %d] => ", job.start, job.required);
-
-			int pending = 0;
-
-			// 추가 대기 시간이 있는가?
-			if (prev > job.start) {
-				pending += prev - job.start;
-			}
-
-			pending += job.required;
-
-			sum += pending;
-			prev += job.required;
-			// System.out.printf("%d만큼 대기 and %d에 종료\n", pending, prev);
-		}
-
-		return sum / jobSize;
 	}
 
-	class Job {
-		public int start;
-		public int required;
+	public int solution(int[][] jobs) {
+		int answer = 0;
 
-		public Job(int start, int required) {
-			this.start = start;
-			this.required = required;
+		// 시작 시간이 빠른 순서대로 내보내주는 우선순위 큐
+		PriorityQueue<Job> startFirst = new PriorityQueue<Job>(new Comparator<Job>() {
+			@Override
+			public int compare(Job o1, Job o2) {
+				return o1.start - o2.start;
+			}
+		});
+
+		for (int[] job : jobs) {
+			startFirst.add(new Job(job[0], job[1]));
 		}
+
+		// 실행 시간이 짧고, 시간이 빠른 순서대로 내보내주는 우선순위 큐
+		PriorityQueue<Job> durationFirst = new PriorityQueue<Job>(new Comparator<Job>() {
+			@Override
+			public int compare(Job o1, Job o2) {
+				if (o1.duration == o2.duration) {
+					return o1.start - o2.start;
+				}
+				return o1.duration - o2.duration;
+			}
+		});
+
+		// k는 현재 시간
+		int k = startFirst.peek().start;
+
+		// 반복이 끝나는 조건은 더 이상 처리할 작업이 없을 때
+		while (!startFirst.isEmpty() || !durationFirst.isEmpty()) {
+			// durationFirst라는 큐에는 실행 가능한 작업만 들어올 수 있음
+			// 실행 가능한 작업이란? 현재 시간(k)보다 시작 시간이 작거나 같은 작업을 의미
+			while (!startFirst.isEmpty() && startFirst.peek().start <= k) {
+				durationFirst.add(startFirst.poll());
+			}
+
+			if (!durationFirst.isEmpty()) {
+				// durationFirst에서 가장 작업 시간이 짧고, 시작 시간이 빠른 애를 뽑는다.
+				Job task = durationFirst.poll();
+				k += task.duration;
+				answer += k - task.start;
+			} else {
+				k = startFirst.peek().start;
+			}
+		}
+		
+		return answer / jobs.length;
 	}
 }
